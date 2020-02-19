9825dcf932b31c1be80a374272ad3f001f45f672
Leonardo <20160527@aloe.ulima.edu.pe>
2020-02-19 00:02:23 -0500

    asdsasd

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ router.get('/',function(req,res){
   
 });
 
-app.use('/', router);
-app.listen(process.env.port || 3000);
+//app.use('/', router);
+
+app.listen(process.env.PORT || 3000);
 
 console.log('Running at Port 3000');
